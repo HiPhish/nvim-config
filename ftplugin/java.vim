f7487dc873753a2dd242545c995101b708d9a112
HiPhish <>
2020-02-06 22:43:42 +0100

    More LSP settings

--- ftplugin/java.vim
+++ ftplugin/java.vim
@@ -47,12 +47,16 @@ endfor
 
 
 " ---[ LanguageClient Neovim settings ]----------------------------------------
-nnoremap <buffer> <silent> <CR>  :call LanguageClient_contextMenu()<CR>
-nnoremap <buffer> <silent> gd    :call LanguageClient#textDocument_definition()<CR>
-nnoremap <buffer> <silent> gO    :call LanguageClient#textDocument_documentSymbol()<CR>
-nnoremap <buffer> <silent> <C-G> :call LanguageClient#textDocument_references()<CR>
-nnoremap <buffer> <silent> <F2>  :call LanguageClient#textDocument_rename()<CR>
-nnoremap <buffer> <silent> K     :call LanguageClient#textDocument_hover()<CR>
+if !has('nvim-0.5.0')
+	nnoremap <buffer> <silent> <CR>  :call LanguageClient_contextMenu()<CR>
+	nnoremap <buffer> <silent> gd    :call LanguageClient#textDocument_definition()<CR>
+	nnoremap <buffer> <silent> gO    :call LanguageClient#textDocument_documentSymbol()<CR>
+	nnoremap <buffer> <silent> <C-G> :call LanguageClient#textDocument_references()<CR>
+	nnoremap <buffer> <silent> <F2>  :call LanguageClient#textDocument_rename()<CR>
+	nnoremap <buffer> <silent> K     :call LanguageClient#textDocument_hover()<CR>
+else
+	nnoremap <buffer> <silent> K :lua vim.lsp.buf.hover()<CR>
+endif
 
 
 " ---[ vim-test integration ]--------------------------------------------------
--- init.lua
+++ init.lua
@@ -1,13 +1,18 @@
+-- See the following for information:
 -- https://github.com/teto/home/blob/98d45eb0e1663aebcec58c1b04f91fde767a5cea/config/nvim/init.lua#L8
 -- https://github.com/neovim/nvim-lsp/issues/41
 
+---[ SERVER CONFIGURATION ]----------------------------------------------------
 local nvim_lsp = require 'nvim_lsp'
 local configs = require'nvim_lsp/configs'
 local util = require'nvim_lsp/util'
 
+-- The workspace directory is generated from the file path
 local workspace = '~/.local/share/eclipse/workspace/' .. vim.call('fnamemodify', vim.call('getcwd'), ':t')
 
-local pom_rp = util.root_pattern("pom.xml")
+-- Root patterns for common build files
+local    ant_rp = util.root_pattern("build.xml")
+local  maven_rp = util.root_pattern("pom.xml")
 local gradle_rp = util.root_pattern("build.gradle")
 
 configs.eclipse_jdt_ls= {
@@ -15,11 +20,16 @@ configs.eclipse_jdt_ls= {
 		cmd = {'sh', vim.loop.os_homedir() .. '/.bin/java-lsp.sh', '-data', workspace};
 		filetypes = {'java'};
 		root_dir = function(fname)
-			return nvim_lsp.util.find_git_ancestor(fname) or pom_rp(fname) or gradle_rp(fname) or vim.loop.os_homedir()
+			return nvim_lsp.util.find_git_ancestor(fname) or ant_rp(fname) or maven_rp(fname) or gradle_rp(fname) or vim.loop.os_homedir()
 		end;
 		log_level = vim.lsp.protocol.MessageType.Warning;
 		settings = {};
 	};
 }
 
+
+---[ SETTING UP SERVERS ]------------------------------------------------------
 nvim_lsp.eclipse_jdt_ls.setup{}
+
+
+---[ EDITOR SETTINGS ]---------------------------------------------------------
