82d96ddd0157087774ec70dceffd3d37346b6c27
HiPhish <>
2020-08-01 22:07:58 +0200

    LSP: Override function for floating window
    Add a default maximum width to the window if there is no such option
    provided.

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,3 +1,31 @@
+local util = require 'vim.lsp.util'
+
+---[ OVERRIDE CLIENT FUNCTIONS ]-----------------------------------------------
+-- Here I override the functions of the LSP client to my liking. An override
+-- should preferably still call the original function for better
+-- maintainability.
+
+-- Store original functions here for back reference. First declare the
+-- function, then assign it wrapped inside a check. The purpose of this
+-- roundabout way is to make sure we do not overwrite the first saved function
+-- if this script gets sourced a second time.
+
+local fancy_floating_markdown
+if not fancy_floating_markdown then
+	fancy_floating_markdown = util['fancy_floating_markdown']
+end
+
+-- Inject a maximum width into the window options.
+util['fancy_floating_markdown'] = function (contents, opts)
+	if opts['max_width'] then goto call end
+	opts['max_width'] = 100  -- I could use a fancy computed value instead
+
+	::call::
+	return fancy_floating_markdown(contents, opts)
+end
+
+
+---[ ECLIPSE.JDT.LS ]----------------------------------------------------------
 local nvim_lsp = require 'nvim_lsp'
 
 -- Load the server configuration into the global table as a side effect
@@ -29,6 +57,5 @@ jdt_ls_config.capabilities.textDocument.codeAction = {
 
 -- Workaround: disable this feature until it gets fixed
 jdt_ls_config.init_options.extendedClientCapabilities.classFileContentsSupport = false
-
 
 nvim_lsp.jdt_ls.setup(jdt_ls_config)
