c45a39f7d34fa81c67cd9c6aaca9c8efcb126a43
HiPhish <hiphish@posteo.de>
2020-11-06 21:37:51 +0100

    LSP/Lua: fix wrong condition check

--- lua/lsp_config/sumneko_lua/rules.lua
+++ lua/lsp_config/sumneko_lua/rules.lua
@@ -8,8 +8,14 @@ local function always()
 	return true
 end
 
+--- There is a local primary LSP settings file.
 local function has_local_settings_file()
 	return vim.fn.filereadable('lua-lsp.json') ~= 0
+end
+
+--- There is a local secondary LSP settings file.
+local function has_local_extra_file()
+	return vim.fn.filereadable('lua-lsp-extra.json') ~= 0
 end
 
 --- Whether the current directory is the Neovim config directory.
@@ -19,22 +25,34 @@ end
 
 
 -- [ RULES ] ------------------------------------------------------------------
---- Rules sorted by priority in descending order
+-- Each rule has a priority which determines whether the rule will be applied:
+-- if the priority of the rule is greater than the current priority, then apply
+-- it and set the current priority to the priority of the rule. This allows
+-- certain rules to block lesser rules from being applied. In particular, this
+-- means that no two rules of equal priority will be both applied.
+
+--- Rules sorted by order of application.
 local rules = {
 	{
 		test = always,
 		settings = settings.default,
-		strict = false,
+		priority = 0,
 	}, {
 		test = has_local_settings_file,
 		settings = function ()
 			vim.fn.json_decode(vim.fn.readfile('lua-lsp.json'))
 		end,
-		strict = true,
+		priority = 3,
 	}, {
 		test = is_nvim_conf_dir,
 		settings = settings.nvim,
-		strict = true,
+		priority = 1,
+	}, {
+		test = has_local_extra_file,
+		settings = function ()
+			vim.fn.json_decode(vim.fn.readfile('lua-lsp-extra.json'))
+		end,
+		priority = 3,
 	}
 }
 
@@ -47,15 +65,14 @@ return {
 
 	apply = function()
 		local result = {}
+		local priority = -1
 		for _, rule in ipairs(rules) do
-			if rule.test() then
+			if priority < rule.priority and rule.test() then
+				priority = rule.priority
 				local current = type(rule.settings) == 'function'
 					and rule.setting()
 					or rule.settings
 				result = vim.tbl_deep_extend('force', result, current)
-				if rule.strict then
-					break
-				end
 			end
 		end
 
