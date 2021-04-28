04a4e0dca9ca5911afda0951a6cce3640e9fb863
HiPhish <hiphish@posteo.de>
2021-04-28 19:26:31 +0200

    DAP: Clean up configuration

--- lua/hiphish/dap.lua
+++ lua/hiphish/dap.lua
@@ -1,34 +1,13 @@
 local dap = require 'dap'
-dap.set_log_level('trace')
 
 local adapters       = dap.adapters
 local configurations = dap.configurations
 
---- [ C FAMILY ]---------------------------------------------------------------
-require 'hiphish.dap.lldb'  -- Defines adapter and sets up command
 
-
---- [ KOTLIN DEBUG ADAPTER ]---------------------------------------------------
-require 'hiphish.dap.kotlin'
-
-
---- [ PYTHON ] ----------------------------------------------------------------
-adapters.python = {
-	type = 'executable',
-	command = '/usr/bin/python3',
-	args = {'-m', 'debugpy.adapter'}
-}
-
-
-configurations.python = {
-	{
-		type = 'python',
-		request = 'launch',
-		name = 'Launch file',
-		program = "${file}",
-		console = 'internalConsole',
-	}
-}
+--- [ OPTIONS ]----------------------------------------------------------------
+vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapLogPoint', {text='🟥', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='🔵', texthl='', linehl='', numhl=''})
 
 
 --- [ KEY MAPPINGS ]-----------------------------------------------------------
@@ -36,7 +15,7 @@ do
 	local opts = {noremap = true, silent = true}
 	local mappings = {
 		{'<F3>' , '<cmd>lua require"dap".toggle_breakpoint()<CR>'},
-		{'<F4>' , '<cmd>lua require"dap".continue()<CR>'         },
+		{'<F5>' , '<cmd>lua require"dap".continue()<CR>'         },
     	{'<F10>', '<cmd>lua require"dap".step_over()<CR>'        },
     	{'<F11>', '<cmd>lua require"dap".step_into()<CR>'        },
     	{'<F12>', '<cmd>lua require"dap".step_out()<CR>'         },
@@ -48,3 +27,43 @@ do
 		vim.api.nvim_buf_set_keymap(0, 'n', lhs, rhs, opts)
 	end
 end
+
+
+--- [ C FAMILY ]---------------------------------------------------------------
+adapters.c = {
+	name = 'lldb-vscode',
+	type = 'executable',
+	attach = {
+		pidProperty = "pid",
+		pidSelect = "ask"
+	},
+	command = 'lldb-vscode',
+	args = {},
+	LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
+	options = {
+		env = {
+			LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
+		},
+	},
+}
+
+adapters.cpp = adapters.c
+
+
+--- [ PYTHON ] ----------------------------------------------------------------
+adapters.python = {
+	name = 'python',
+	type = 'executable',
+	command = '/usr/bin/python3',
+	args = {'-m', 'debugpy.adapter'}
+}
+
+configurations.python = {
+	{
+		type = 'python',
+		request = 'launch',
+		name = 'Launch file',
+		program = "${file}",
+		console = 'internalConsole',
+	}
+}
