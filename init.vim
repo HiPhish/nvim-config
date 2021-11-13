3abdf085e6803596db91866efcbf7a0f9e767709
HiPhish <hiphish@posteo.de>
2021-11-13 01:02:33 +0100

    Consolidate statusline plugin file

--- init.vim
+++ init.vim
@@ -93,7 +93,3 @@ elseif $TERM_PROGRAM =~? 'iTerm'
 	let &t_SI = "\<Esc>]50;CursorShape=1\x7" " Vertical bar in insert mode
 	let &t_EI = "\<Esc>]50;CursorShape=0\x7" " Block in normal mode
 endif
-
-
-" ---[ LUA CONFIGURATION ]-----------------------------------------------------
-lua require 'hiphish.lualine'
--- lua/hiphish/lualine/components/gps.lua
+++ lua/hiphish/lualine/components/gps.lua
@@ -4,7 +4,22 @@ if not success then
 	return {}
 end
 
+gps.setup {
+	icons = {
+		['class-name'] = '',
+		['function-name'] = '',
+		['method-name'] = '',
+		['container-name'] = '',
+		['tag-name'] = '',
+	},
+	languages = {
+		vim = false,
+	},
+	separator = ' ╱ ',
+	depth_limit_indicator = '…'
+}
+
 return {
 	gps.get_location,
-	condition = gps.is_available,
+	cond = gps.is_available,
 }
