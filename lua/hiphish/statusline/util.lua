db3d4e2807cd87118c36ca1a867af795d723f051
HiPhish <hiphish@posteo.de>
2022-01-30 22:02:29 +0100

    Statusline: improve mode indicator display
    - Add some of the missing modes
    - If a mode does not have an exact match fall back on the first letter

--- lua/hiphish/statusline/util.lua
+++ lua/hiphish/statusline/util.lua
@@ -3,27 +3,40 @@ local M = {}
 local api = vim.api
 
 M.modes = {
-	n      = {label = 'NORMAL'        , short = ' N ', hi = 'Normal'},
-	no     = {label = 'NORMAL'        , short = ' N ', hi = 'Normal'},
-	nt     = {label = 'NORMAL'        , short = ' N ', hi = 'Normal'},
-	v      = {label = 'VISUAL'        , short = ' v ', hi = 'Visual'},
-	V      = {label = 'VISULA LINE'   , short = ' V ', hi = 'Visual'},
-	[''] = {label = 'VISUAL BLOCK'  , short = '[V]', hi = 'Visual'},
-	s      = {label = 'SELECT'        , short = ' s ', hi = 'Select'},
-	S      = {label = 'SELECT LINE'   , short = ' S ', hi = 'Select'},
-	[''] = {label = 'SELECT BLOCK'  , short = '[S]', hi = 'Select'},
-	i      = {label = 'INSERT'        , short = ' i ', hi = 'Insert'},
-	ic     = {label = 'INSERT'        , short = ' i ', hi = 'Insert'},
-	R      = {label = 'REPLACE'       , short = ' r ', hi = 'Replace'},
-	Rv     = {label = 'VISUAL REPLACE', short = ' r ', hi = 'Replace'},
-	c      = {label = 'COMMAND'       , short = ' c ', hi = 'Cmd'},
-	cv     = {label = 'VIM EX'        , short = ' c ', hi = 'Cmd'},
-	ce     = {label = 'EX'            , short = ' c ', hi = 'Cmd'},
-	r      = {label = 'PROMPT'        , short = ' c ', hi = 'Cmd'},
-	rm     = {label = 'MORE'          , short = ' c ', hi = 'Cmd'},
-	['r?'] = {label = 'CONFIRM'       , short = ' c ', hi = 'Cmd'},
-	['!']  = {label = 'SHELL'         , short = ' c ', hi = 'Cmd'},
-	t      = {label = 'TERMINAL'      , short = ' t ', hi = 'Insert'},
+	n        = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	no       = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	nov      = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	noV      = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	['no'] = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	niI      = {label = '(NORMAL)'       , short = ' N ', hi = 'Normal'},
+	niR      = {label = '(NORMAL)'       , short = ' N ', hi = 'Normal'},
+	niV      = {label = '(NORMAL)'       , short = ' N ', hi = 'Normal'},
+	nt       = {label = 'NORMAL'         , short = ' N ', hi = 'Normal'},
+	v        = {label = 'VISUAL'         , short = ' v ', hi = 'Visual'},
+	vs       = {label = 'VISUAL'         , short = ' v ', hi = 'Visual'},
+	V        = {label = 'VISULA LINE'    , short = ' V ', hi = 'Visual'},
+	['']   = {label = 'VISUAL BLOCK'   , short = '[V]', hi = 'Visual'},
+	['s']  = {label = 'VISUAL BLOCK'   , short = '[V]', hi = 'Visual'},
+	s        = {label = 'SELECT'         , short = ' s ', hi = 'Select'},
+	S        = {label = 'SELECT LINE'    , short = ' S ', hi = 'Select'},
+	['']   = {label = 'SELECT BLOCK'   , short = '[S]', hi = 'Select'},
+	i        = {label = 'INSERT'         , short = ' i ', hi = 'Insert'},
+	ic       = {label = 'INSERT'         , short = ' i ', hi = 'Insert'},
+	ix       = {label = 'INSERT'         , short = ' i ', hi = 'Insert'},
+	R        = {label = 'REPLACE'        , short = ' r ', hi = 'Replace'},
+	Rc       = {label = 'VISUAL REPLACE' , short = ' r ', hi = 'Replace'},
+	Rx       = {label = 'VISUAL REPLACE' , short = ' r ', hi = 'Replace'},
+	Rv       = {label = 'VIRTUAL REPLACE', short = ' r ', hi = 'Replace'},
+	Rvc      = {label = 'VIRTUAL REPLACE', short = ' r ', hi = 'Replace'},
+	Rvx      = {label = 'VIRTUAL REPLACE', short = ' r ', hi = 'Replace'},
+	c        = {label = 'COMMAND'        , short = ' c ', hi = 'Cmd'},
+	cv       = {label = 'VIM EX'         , short = ' c ', hi = 'Cmd'},
+	ce       = {label = 'EX'             , short = ' c ', hi = 'Cmd'},
+	r        = {label = 'PROMPT'         , short = ' c ', hi = 'Cmd'},
+	rm       = {label = 'MORE'           , short = ' c ', hi = 'Cmd'},
+	['r?']   = {label = 'CONFIRM'        , short = ' c ', hi = 'Cmd'},
+	['!']    = {label = 'SHELL'          , short = ' c ', hi = 'Cmd'},
+	t        = {label = 'TERMINAL'       , short = ' t ', hi = 'Insert'},
 }
 
 -- Links the `StatusLineAccentMode` highlight group to the group of the current
@@ -32,7 +45,9 @@ M.modes = {
 -- allocations.
 function M.hi_statusline_accent_mode()
 	local current_mode = api.nvim_get_mode().mode
-	local spec = M.modes[current_mode]
+	-- If there is no match for the exact mode it is safe to compare the first
+	-- letter only according to `:h mode()`
+	local spec = M.modes[current_mode] or M.modes[current_mode:sub(1, 1)]
 	if not spec then return end
 	local hi = spec.hi or ''
 	vim.cmd(string.format('hi link StatusLineAccentMode StatusLineAccent%s', hi))
