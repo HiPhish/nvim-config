f5ee3117c235b8b8fb6e44a7568894033bf4b819
HiPhish <hiphish@posteo.de>
2022-04-25 21:13:30 +0200

    TS: Remove context_commentstring setting
    I have no idea what that is of where it comes from. Maybe from some
    older version of the plugin?

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -10,9 +10,6 @@ require'nvim-treesitter.configs'.setup {
 		enable = true,
 	},
 	indent = {
-		enable = true
-	},
-	context_commentstring = {
 		enable = true,
 		disable = {'python', 'yaml'},
 	},
