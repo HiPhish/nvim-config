2e4c09ca5c9e1ad708da24bdee965b9b474f3879
HiPhish <hiphish@posteo.de>
2022-06-12 01:19:32 +0200

    Simplify diagnostic settings

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -1,8 +1,6 @@
 -- Settings for Neovim's diagnostic framework, see :h diagnostic.txt
 
 
-local api = vim.api
-
 --- [ HIGHLIGHT AND SIGNS ] ---------------------------------------------------
 vim.fn.sign_define {
 	{name='DiagnosticSignError', text='⛔', texthl='DiagnosticSignError', linehl='', numhl=''},
@@ -10,14 +8,10 @@ vim.fn.sign_define {
 	{name='DiagnosticSignInfo' , text='ℹ️' , texthl='DiagnosticSignInfo' , linehl='', numhl=''},
 	{name='DiagnosticSignHint' , text='💡', texthl='DiagnosticSignHint' , linehl='', numhl=''},
 }
--- vim.cmd [[
--- 	sign define DiagnosticSignError text=⛔ texthl=DiagnosticSignError linehl= numhl=
--- 	sign define DiagnosticSignWarn  text=⚠️  texthl=DiagnosticSignWarn  linehl= numhl=
--- 	sign define DiagnosticSignInfo  text=ℹ️  texthl=DiagnosticSignInfo  linehl= numhl=
--- 	sign define DiagnosticSignHint  text=💡 texthl=DiagnosticSignHint  linehl= numhl=
--- ]]
 
 
 --- [ KEY MAPPINGS ]-----------------------------------------------------------
-api.nvim_set_keymap('n', 'g?', '<CMD>lua require("hiphish.diagnostic").open_float()<CR>',
-	{noremap = true, silent = true})
+local function open_float()
+	vim.diagnostic.open_float {border = 'rounded'}
+end
+vim.keymap.set('n', 'g?', open_float, {silent = true})
