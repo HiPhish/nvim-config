dd5a155ef9a2d8578cae119ff606a71285f39a73
Aleksandar Sandic <aleksandar.sandic@ambient.digital>
2023-06-06 09:06:30 +0200

    Fix colours of Python operator conceals

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,17 +1,17 @@
 ;; extends
 
 ;; --- [ MORE TERSE OPERATORS ]------------------------------------------------
-(("not" @TSOperator) (#set! conceal "¬"))
-(("and" @TSOperator) (#set! conceal "∧"))
-(("or"  @TSOperator) (#set! conceal "∨"))
-(("in"  @TSOperator) (#set! conceal "∈"))
-(("=="  @TSOperator) (#set! conceal "≡"))
-(("<="  @TSOperator) (#set! conceal "≤"))
-((">="  @TSOperator) (#set! conceal "≥"))
-(("!="  @TSOperator) (#set! conceal "≠"))
-(("->"  @TSOperator) (#set! conceal "→"))
+(("not" @keyword.operator.python) (#set! conceal "¬"))
+(("and" @keyword.operator.python) (#set! conceal "∧"))
+(("or"  @keyword.operator.python) (#set! conceal "∨"))
+(("in"  @keyword.operator.python) (#set! conceal "∈"))
+(("=="  @operator.python) (#set! conceal "≡"))
+(("<="  @operator.python) (#set! conceal "≤"))
+((">="  @operator.python) (#set! conceal "≥"))
+(("!="  @operator.python) (#set! conceal "≠"))
+(("->"  @operator.python) (#set! conceal "→"))
 
-(("lambda" @TSKeywordFunction) (#set! conceal "λ"))
+(("lambda" @keyword.function.python) (#set! conceal "λ"))
 
 ;; Other ideas: https://gist.github.com/graelo/2d1f5bc6775432a5c8eb6d6c8b624011
 ;;  - all()   -> ∀
@@ -25,44 +25,44 @@
 ;; --- [ LESS NOISY COMPREHENSIONS ]-------------------------------------------
 ([(list_comprehension
     (for_in_clause
-      "for" @TSRepeat))
+      "for" @repeat.python))
   (set_comprehension
     (for_in_clause
-      "for" @TSRepeat))
+      "for" @repeat.python))
   (dictionary_comprehension
     (for_in_clause
-      "for" @TSRepeat))
+      "for" @repeat.python))
   (generator_expression
     (for_in_clause
-      "for" @TSRepeat))]
+      "for" @repeat.python))]
  (#set! conceal "∀"))
 
 ([(list_comprehension
     (for_in_clause
-      "in" @TSRepeat))
+      "in" @keyword.operator.python))
   (set_comprehension
     (for_in_clause
-      "in" @TSRepeat))
+      "in" @keyword.operator.python))
   (dictionary_comprehension
     (for_in_clause
-      "in" @TSRepeat))
+      "in" @keyword.operator.python))
   (generator_expression
     (for_in_clause
-      "in" @TSRepeat))]
+      "in" @keyword.operator.python))]
  (#set! conceal "∈"))
 
 ([(list_comprehension
     (if_clause
-      "if" @TSRepeat))
+      "if" @conditional.python))
   (set_comprehension
     (if_clause
-      "if" @TSRepeat))
+      "if" @conditional.python))
   (dictionary_comprehension
     (if_clause
-      "if" @TSRepeat))
+      "if" @conditional.python))
   (generator_expression
     (if_clause
-      "if" @TSRepeat))]
+      "if" @conditional.python))]
  (#set! conceal "|"))
 
 
