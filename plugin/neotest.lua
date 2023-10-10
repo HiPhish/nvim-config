537473be1228611fbcd00090a37422b2bac2c323
HiPhish <hiphish@posteo.de>
2023-10-10 21:09:04 +0200

    Update Neotest configuration

--- plugin/neotest.lua
+++ plugin/neotest.lua
@@ -3,7 +3,10 @@ if not success then return end
 
 local adapters = {
 	require 'neotest-python' {
-		dap = {justMyCode = false}
+		runner = 'pytest',
+		dap = {
+			justMyCode = false
+		},
 	},
 }
 
@@ -14,15 +17,103 @@ end)
 
 neotest.setup {
 	adapters = adapters,
+	consumers = {},
+	diagnostic = {
+		enabled = true,
+		severity = 1
+	},
+	floating = {
+		border = "rounded",
+		max_height = 0.6,
+		max_width = 0.6,
+		options = {}
+	},
+	highlights = {
+		adapter_name = 'NeotestAdapterName',
+		border = 'NeotestBorder',
+		dir = 'NeotestDir',
+		expand_marker = 'NeotestExpandMarker',
+		failed = 'NeotestFailed',
+		file = 'NeotestFile',
+		focused = "NeotestFocused",
+		indent = 'NeotestIndent',
+		marked = 'NeotestMarked',
+		namespace = 'NeotestNamespace',
+		passed = 'NeotestPassed',
+		running = 'NeotestRunning',
+		select_win = 'NeotestWinSelect',
+		skipped = 'NeotestSkipped',
+		target = 'NeotestTarget',
+		test = 'NeotestTest',
+		unknown = 'NeotestUnknown',
+		watching = 'NeotestWatching',
+	},
 	icons = {
 		skipped = '-',
 	},
+	log_level = 3,
+	projects = {},
+	state = {
+		enabled = true
+	},
+	status = {
+		enabled = true,
+		signs = true,
+		virtual_text = false
+	},
+	strategies = {
+		integrated = {
+			height = 40,
+			width = 120,
+		},
+	},
 	summary = {
+		animated = true,
+		enabled = true,
 		expand_errors = true,
+		follow = true,
+		mappings = {
+			attach = "a",
+			clear_marked = "M",
+			clear_target = "T",
+			debug = "d",
+			debug_marked = "D",
+			expand = { "<CR>", "<2-LeftMouse>" },
+			expand_all = "e",
+			jumpto = "i",
+			mark = "m",
+			next_failed = "J",
+			output = "o",
+			prev_failed = "K",
+			run = "r",
+			run_marked = "R",
+			short = "O",
+			stop = "u",
+			target = "t",
+			watch = "w"
+		},
+		open = "botright vsplit | vertical resize 50"
 	},
 	output = {
+		enabled = true,
 		open_on_run = true
 	},
+	output_panel = {
+		enabled = true,
+		open = "botright split | resize 15"
+	},
+	quickfix = {
+		enabled = true,
+		open = false
+	},
+	watch = {
+		enabled = true,
+		symbol_queries = {
+		-- elixir = <function 1>,
+		-- lua = '		;query\n		;Captures module names in require calls\n		(function_call\n		  name: ((identifier) @function (#eq? @function "require"))\n		  arguments: (arguments (string) @symbol))\n	  ',
+		-- python = "		;query\n		;Captures imports and modules they're imported from\n		(import_from_statement (_ (identifier) @symbol))\n		(import_statement (_ (identifier) @symbol))\n	  "
+	  }
+	}
 }
 
 ---[ NEOTEST COMMAND ]---------------------------------------------------------
@@ -33,14 +124,14 @@ local sep = '\\v[^\\\\](\\\\\\\\)*\\zs\\s+'
 
 local subcommands = {
 	run = function()
-		neotest.run.run()
+		neotest.run.run {suite = false}
 	end,
 	stop = function()
 		local config = {}
 		neotest.run.stop(config)
 	end,
 	debug = function()
-		neotest.run.run {strategy = "dap"}
+		neotest.run.run {suite = false, strategy = 'dap'}
 	end,
 	summary = function(fargs)
 		local how = fargs[1] or 'toggle'
@@ -56,6 +147,11 @@ local subcommands = {
 	end,
 	output = function ()
 		neotest.output.open {
+			short = true,
+			enter = false,
+			quite = false,
+			last_run = true,
+			auto_close = true,
 		}
 	end
 }
@@ -100,4 +196,4 @@ local function neotest_cmd(args)
 	end
 end
 
-vim.api.nvim_create_user_command('Neotest', neotest_cmd, {nargs='*', complete=complete_cmd})
+vim.api.nvim_create_user_command('Ntest', neotest_cmd, {nargs='*', complete=complete_cmd})
