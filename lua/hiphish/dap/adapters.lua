db075aae41803b41d5cefca4907e81afa4f96b0a
HiPhish <hiphish@posteo.de>
2024-02-18 19:30:43 +0100

    Add DAP adapter config for Lua

--- lua/hiphish/dap/adapters.lua
+++ lua/hiphish/dap/adapters.lua
@@ -21,4 +21,26 @@ M.c = {
 
 M.cpp = M.c
 
+-- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#local-lua-debugger-vscode
+M['local-lua'] = {
+	name = 'local-lua',
+	type = 'executable',
+	command = 'node',
+	args = {
+		vim.env.HOME .. '/Applications/dap/local-lua-debugger-vscode/extension/debugAdapter.js'
+	},
+	enrich_config = function(config, on_config)
+		if not config['extensionPath'] then
+			local c = vim.deepcopy(config)
+			-- If this is missing or wrong you'll see "module 'lldebugger' not
+			-- found" errors in the dap-repl when trying to launch a debug
+			-- session.
+			c.extensionPath = vim.env.HOME .. '/Applications/dap/local-lua-debugger-vscode'
+			on_config(c)
+		else
+			on_config(config)
+		end
+	end
+}
+
 return M
