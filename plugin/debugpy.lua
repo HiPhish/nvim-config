33fe2050df189b8326648d4e7884d3f209c54c56
HiPhish <hiphish@posteo.de>
2024-03-20 17:50:25 +0100

    Fix debugpy configuration
    Do not replace the `debugpy.run` function, decorate it instead.

--- plugin/debugpy.lua
+++ plugin/debugpy.lua
@@ -1,12 +1,14 @@
-local dap     = require 'dap'
 local debugpy = require 'debugpy'
 
 local default_config = {
 	justMyCode = false,
 	django = true;
+	cwd = vim.fn.getcwd(),
 }
+
+local old_run = debugpy.run
 
 debugpy.run = function(config)
 	-- Extend the configuration with my personal defaults
-	dap.run(vim.tbl_extend('keep', config, default_config))
+	old_run(vim.tbl_extend('keep', config, default_config))
 end
