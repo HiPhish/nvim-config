d52d4434f7021654fa70c216b0861bb896e552c8
HiPhish <hiphish@posteo.de>
2024-11-09 10:02:47 +0100

    Fix up custom Lua concealment queries

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -5,12 +5,12 @@
 (("and" @keyword.operator.lua) (#set! conceal "∧"))
 (("or"  @keyword.operator.lua) (#set! conceal "∨"))
 (("in"  @keyword.operator.lua) (#set! conceal "∈"))
-(("<="  @operator.) (#set! conceal "≤"))
-((">="  @operator.) (#set! conceal "≥"))
-(("~="  @operator.) (#set! conceal "≠"))
+(("<="  @operator.lua) (#set! conceal "≤"))
+((">="  @operator.lua) (#set! conceal "≥"))
+(("~="  @operator.lua) (#set! conceal "≠"))
 
 (for_statement
-  "for" @repeat.lua (#set! conceal "∀"))
+  "for" @keyword.repeat.lua (#set! conceal "∀"))
 
 (function_definition
   "function" @keyword.function.lua (#set! conceal "λ"))
