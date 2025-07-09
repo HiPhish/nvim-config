0db335f16202ed80dba540b88aa1c63109238881
HiPhish <hiphish@posteo.de>
2025-07-09 23:44:57 +0200

    Update highlights query for Common Lisp

--- after/queries/commonlisp/highlights.scm
+++ after/queries/commonlisp/highlights.scm
@@ -1,10 +1,25 @@
 ;; extends
 
 ;; --- [ MORE TERSE OPERATORS ]------------------------------------------------
-(((defun_header
-    keyword:(defun_keyword) @TSFuncMacro
-    (#eq? @TSFuncMacro "lambda")))
+((defun_header
+   keyword: (defun_keyword) @function.macro.commonlisp
+            (#eq? @function.macro.commonlisp "lambda"))
  (#set! conceal "λ"))
+
+((list_lit
+   . value: (sym_lit) @function.builtin.commonlisp
+            (#match? @function.builtin.commonlisp "^[aA][nN][dD]$"))
+ (#set! conceal "∧"))
+
+((list_lit
+   . value: (sym_lit) @function.builtin.commonlisp
+            (#match? @function.builtin.commonlisp "^[oO][rR]$"))
+ (#set! conceal "∨"))
+
+((list_lit
+   . value: (sym_lit) @function.builtin.commonlisp
+            (#match? @function.builtin.commonlisp "^[nN][oO][tT]$"))
+ (#set! conceal "¬"))
 
 
 ;; ----------------------------------------------------------------------------
