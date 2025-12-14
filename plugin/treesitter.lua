abb344997e1a5be9a6bd88bbeb31e8d48b229366
HiPhish <hiphish@posteo.de>
2025-12-14 23:38:52 +0100

    Remove obsolete Tree-sitter configuration
    Following the introduction of `lsp.config` and on the `main` branch of
    `nvim-treesitter` the old settings no longer apply.

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,48 +1,10 @@
--- Context comment string
--- See |ts-context-commentstring.txt|
-vim.g.skip_ts_context_commentstring_module = true
-
-
-require'nvim-treesitter.configs'.setup {
-	highlight = {
-		enable = true,
-		use_languagetree = true, -- Use this to enable language injection
-		custom_captures = {
-			-- List of `["capture"] = "Vim HL group"` mappings
-		},
-	},
-	incremental_selection = {
-		enable = true,
-	},
-	indent = {
-		enable = true,
-		disable = {'python', 'yaml'},
-	},
-	matchup = {
-		enable = true,
-		disable = {},
-	},
-	-- I want to be able to set mappings per file type, but the textobject
-	-- plugin does not support that. I should look for a better tree
-	-- manipulation plugin instead.
-	textobjects = {
-		swap = {
-			enable = true,
-			swap_next = {
-				['<m-l>'] = '@SExpr',
-			},
-			swap_previous = {
-				['<m-h>'] = '@SExpr',
-			}
-		}
-	}
-}
-
-do  -- Fix spell checking
-	local success, spellsitter = pcall(require, 'spellsitter')
-	if success then
-		spellsitter.setup {
-			captures = {'comment', 'string'}
-		}
+-- Auto-start Tree-sitter highlighting
+vim.api.nvim_create_autocmd('BufWinEnter', {
+	pattern = '*',
+	callback = function(_args)
+		if vim.bo.filetype ~= '' then
+			-- Silently ignore failures
+			pcall(vim.treesitter.start)
+		end
 	end
-end
+})
