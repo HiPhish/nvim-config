4394c031be9cfadd4dac361529051b80ec57372b
HiPhish <hiphish@posteo.de>
2026-04-17 12:21:21 +0200

    Disable rainbow delimiters for large XML files

--- plugin/rainbow.lua
+++ plugin/rainbow.lua
@@ -1,7 +1,16 @@
+local api = vim.api
+
 ---@type rainbow_delimiters.config
 vim.g.rainbow_delimiters = {
 	strategy = {
 		commonlisp = 'rainbow-delimiters.strategy.local',
+		xml = function(bufnr)
+			-- Disable for large files
+			if api.nvim_buf_line_count(bufnr) > 10000 then
+				return
+			end
+			return 'rainbow-delimiters.strategy.global'
+		end,
 	},
 	query = {
 		query = function (bufnr)
